2544aa2281b29a0d95cbdbd92398276f9fc7a376
Ly-e <ly04120921@gmail.com>
2023-02-17 16:15:43 +0800

    创建三个页面details、statistics、keepone

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,44 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Index from '@/views/Index.vue'
+import KeepOne from '@/views/KeepOne.vue'
+import StatisticsPage from '@/views/StatisticsPage.vue'
+import DetailsPage from '@/views/DetailsPage.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: Index
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/index',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '/keepone',
+    name: 'KeepOne',
+    component: KeepOne
+  },
+  {
+    path: '/details',
+    name: 'DetailsPage',
+    component: DetailsPage
+  },
+  {
+    path: '/statistics',
+    name: 'StatisticsPage',
+    component: StatisticsPage
+  },
+  {
+    path: '/notfound',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/KeepOne.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+       KeepOne.vue
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
