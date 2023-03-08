ba5f3381795fb8a6f348c8af5d99b8c8779edc94
Ly-e <1091471466@qq.com>
2023-03-08 23:06:24 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,15 @@
 const localStorageKeyName = 'recordList';
 const recordLisModel = {
+    data: [] as RecordItem[],
     fetch() {
         return JSON.parse(
             window.localStorage.getItem(localStorageKeyName) || "[]"
         ) as RecordItem[];
     },
-    save(data: RecordItem[]) {
+    save() {
         window.localStorage.setItem(
             localStorageKeyName,
-            JSON.stringify(data)
+            JSON.stringify(this.data)
         );
     },
     clone(data: RecordItem | RecordItem[]) {
